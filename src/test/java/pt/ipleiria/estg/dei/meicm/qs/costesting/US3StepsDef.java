a959595a11b682521256b66a28ec06cbbb0f550e
Bruno <2172321@my.ipleiria.pt>
2018-04-17 19:21:54 +0100

    US3 steps and scenarios

--- src/test/java/pt/ipleiria/estg/dei/meicm/qs/costesting/US3StepsDef.java
+++ src/test/java/pt/ipleiria/estg/dei/meicm/qs/costesting/US3StepsDef.java
@@ -1,7 +1,18 @@
 package pt.ipleiria.estg.dei.meicm.qs.costesting;
 
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class US3StepsDef {
     private PhantomJSDriver driver;
@@ -18,5 +29,32 @@ public class US3StepsDef {
 
 
         driver = new PhantomJSDriver();
+        driver.get("http://35.187.16.192/COSProject/index.php");
     }
+
+
+
+    @When("^i type \"([^\"]*)\" on the input box$")
+    public void iTypeOnTheInputBox(String name) throws Throwable {
+        WebElement searchbox = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='data-table_filter']/label/input")));
+        // Write code here that turns the phrase above into concrete actions
+       // WebElement element = driver.findElement(By.xpath("//div[@id='data-table_filter']/label/input"));
+        searchbox.sendKeys(name);
+    }
+
+    @Then("^the result should be \"([^\"]*)\" user$")
+    public void theFieldIsFilledOutWith(int aux) throws Throwable {
+        int i = driver.findElements(By.xpath("//table[@id='data-table']/tbody/tr")).size();
+        assertEquals(aux, i);
+    }
+
+
+    @Then("^the result should be \"([^\"]*)\" error message$")
+    public void theResultShouldBe(String results) throws Throwable {
+        assertEquals(driver.findElement(By.xpath("//table[@id='data-table']/tbody/tr/td")).getText(),results);
+    }
+
+
+
 }
--- src/test/java/resources/US3.feature
+++ src/test/java/resources/US3.feature
@@ -2,3 +2,13 @@ Feature: Access to the Contacts Orchestrator Solution's (COS) Landing Page
   As a user
   I want to search users by name or Guid
   So that I can find a specific user
+
+  Scenario: Search a valid User
+    Given I am on the Contact List page
+    When i type "Joao " on the input box
+    Then the result should be "3" user
+  
+  Scenario: Search an invalid User
+    Given I am on the Contact List page
+    When i type "Joooooo" on the input box
+    Then the result should be "No matching records found" error message
