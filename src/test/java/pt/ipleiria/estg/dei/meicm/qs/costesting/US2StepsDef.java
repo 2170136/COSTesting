1b1d7f0711c3ef8c6a37b97c3a96b0e1dc15d105
José Guerra <2170136@my.ipleiria.pt>
2018-04-17 19:35:09 +0100

    US2

--- src/test/java/pt/ipleiria/estg/dei/meicm/qs/costesting/US2StepsDef.java
+++ src/test/java/pt/ipleiria/estg/dei/meicm/qs/costesting/US2StepsDef.java
@@ -1,5 +1,6 @@
 package pt.ipleiria.estg.dei.meicm.qs.costesting;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -8,6 +9,13 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class US2StepsDef {
     private PhantomJSDriver driver;
@@ -57,6 +65,87 @@ public class US2StepsDef {
             String phone = element.getText();
             //phone = "97";
             Assert.assertTrue(phone.matches("^[0-9]{9}$"));
+        }
+        else if(arg0.equals("Birthday"))
+        {
+            WebElement element = driver.findElement(By.xpath("//span[@id='birthday_value']"));
+            String birthday = element.getText();
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            try {
+                Date date = formatter.parse(birthday);
+                Date dateNow = new Date();
+                Assert.assertTrue(date.before(dateNow));
+            }catch (Exception e)
+            {
+                Assert.assertTrue(1==2);
+            }
+        }
+        else if(arg0.equals("Guid"))
+        {
+            WebElement element = driver.findElement(By.xpath("//span[@id='guid_value']"));
+            String Guid = element.getText();
+            //Guid = "021a1dc3-5b75-4868-bb03-333170ce9ac";
+
+            Assert.assertTrue(Guid.matches("^[a-zA-Z0-9]{8}+-+[a-zA-Z0-9]{4}+-+[a-zA-Z0-9]{4}+-+[a-zA-Z0-9]{4}+-+[a-zA-Z0-9]{12}$"));
+        }
+        else if(arg0.equals("photourl"))
+        {
+            WebElement element = driver.findElement(By.xpath("//span[@id='photo_url_value']"));
+            String photoutl = element.getText();
+            //Guid = "021a1dc3-5b75-4868-bb03-333170ce9ac";
+            try {
+                BufferedImage image = ImageIO.read(new URL(photoutl));
+                //BufferedImage image = ImageIO.read(new URL("http://someimage.jpg"));
+                if (image != null) {
+                    Assert.assertTrue(1==1);
+                } else {
+                    Assert.assertTrue(2==1);
+                }
+            } catch(Exception e)
+            {
+                Assert.assertTrue(2==1);
+            }
+
+        }
+    }
+
+    @Then("^the field \"([^\"]*)\" should exits$")
+    public void theFieldExits(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        if(arg0.equals("contact_name")){
+            WebElement element = driver.findElement(By.xpath("//h2[@id='contact_name']"));
+            String contactName = element.getText();
+            Assert.assertTrue(contactName.length()>0);
+        }
+        if(arg0.equals("giveName")){
+            WebElement element = driver.findElement(By.xpath("//span[@id='given_name_value']"));
+            String giveName = element.getText();
+            Assert.assertTrue(giveName.length()>0);
+        }
+        if(arg0.equals("surName")){
+            WebElement element = driver.findElement(By.xpath("//span[@id='surname_value']"));
+            String surName = element.getText();
+            Assert.assertTrue(surName.length()>0);
+        }
+        if(arg0.equals("occupation")){
+            WebElement element = driver.findElement(By.xpath("//span[@id='occupation_value']"));
+            String occupation = element.getText();
+            Assert.assertTrue(occupation.length()>0);
+        }
+        if(arg0.equals("streetAddress")){
+            WebElement element = driver.findElement(By.xpath("//span[@id='street_address_value']"));
+            String streetAddress = element.getText();
+            Assert.assertTrue(streetAddress.length()>0);
+        }
+        if(arg0.equals("city")){
+            WebElement element = driver.findElement(By.xpath("//span[@id='city_value']"));
+            String city = element.getText();
+            Assert.assertTrue(city.length()>0);
+        }
+        if(arg0.equals("company")){
+            WebElement element = driver.findElement(By.xpath("//span[@id='company_value']"));
+            String company = element.getText();
+            Assert.assertTrue(company.length()>0);
         }
     }
 }
--- src/test/java/resources/US2.feature
+++ src/test/java/resources/US2.feature
@@ -12,3 +12,58 @@ Feature: Access to the Contacts Orchestrator Solution's (COS) Landing Page
     Given I am on the Contact List page
     When i click in the first more info button
     Then the field "phone" should be in the right format
+
+  Scenario: Check Birthday Format
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "Birthday" should be in the right format
+
+  Scenario: Check Guid Format
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "Guid" should be in the right format
+
+  Scenario: Check Guid Format
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "Guid" should be in the right format
+
+  Scenario: Check Contact Name
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "contact_name" should exits
+
+  Scenario: Check Given Name
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "giveName" should exits
+
+  Scenario: Check Sur Name
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "surName" should exits
+
+  Scenario: Check Occupation
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "occupation" should exits
+
+  Scenario: Check Street Address
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "streetAddress" should exits
+
+  Scenario: Check City Address
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "city" should exits
+
+  Scenario: Check Company
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "company" should exits
+
+  Scenario: Check Photo Url
+    Given I am on the Contact List page
+    When i click in the first more info button
+    Then the field "photourl" should be in the right format
