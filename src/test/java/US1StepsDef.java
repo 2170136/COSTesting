81802fd68fcac16512dc11d77f502fd68451fc81
José Guerra <2170136@my.ipleiria.pt>
2018-05-10 14:57:27 +0100

    US5

--- src/test/java/US1StepsDef.java
+++ src/test/java/US1StepsDef.java
@@ -61,7 +61,7 @@ public class US1StepsDef {
         System.setProperty("webdriver.chrome.driver",
                 "drivers\\chromedriver.exe");
         System.setProperty("phantomjs.binary.path",
-                "drivers\\phantomjs");
+                "drivers/phantomjs");
 
        // String phantomjsExeutableFilePath = "usr\\drivers\\phantomjs";
         //System.setProperty("phantomjs.binary.path", phantomjsExeutableFilePath);
--- src/test/java/US3StepsDef.java
+++ src/test/java/US3StepsDef.java
@@ -31,7 +31,7 @@ public class US3StepsDef {
         System.setProperty("webdriver.chrome.driver",
                 "drivers\\chromedriver.exe");
         System.setProperty("phantomjs.binary.path",
-                "drivers\\phantomjs");
+                "drivers/phantomjs");
 
 
         driver = new PhantomJSDriver();
